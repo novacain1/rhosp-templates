5e6da27f5d811f864172938c98d0ae9b67f895b0
Dave Cain <novacain1@users.noreply.github.com>
2018-04-24 14:26:49 -0400

    Update postdeploy.sh

--- JRCN/postdeploy-scripts/postdeploy.sh
+++ JRCN/postdeploy-scripts/postdeploy.sh
@@ -80,9 +80,9 @@ cinder quota-update --volumes 500 --gigabytes 216000 $tenant
 neutron quota-update --tenant_id $tenant --port 100000 --floatingip 200
 
 #create custom flavors
-openstack flavor create --public baremetal --id auto --ram 1024 --disk 20 --vcpus 1 --property baremetal=true --public
-openstack flavor create --public m1.tiny --id auto --ram 512 --disk 1 --property baremetal=false --public
-openstack flavor create --public m1.medium --id auto --ram 4096 --disk 10 --property baremetal=false --public
+#openstack flavor create --public baremetal --id auto --ram 1024 --disk 20 --vcpus 1 --property baremetal=true --public
+openstack flavor create --public m1.tiny --id auto --ram 512 --disk 1 --public
+openstack flavor create --public m1.medium --id auto --ram 4096 --disk 10 --public
 #openstack flavor create --id auto --ram 1024 --disk 10 --vcpus 2 dpdk-flavor.s1 --public
 #openstack flavor set \
 #  --property hw:mem_page_size=large \
@@ -93,11 +93,11 @@ openstack flavor create --public m1.medium --id auto --ram 4096 --disk 10 --prop
 #  dpdk-flavor.s1
 
 #aggregates and hosts
-openstack aggregate create --property baremetal=true baremetal-hosts
-openstack aggregate add host baremetal-hosts server-1-5
-openstack aggregate add host baremetal-hosts server-1-6
-openstack aggregate add host baremetal-hosts server-1-7
-openstack aggregate add host baremetal-hosts server-1-8
+#openstack aggregate create --property baremetal=true baremetal-hosts
+#openstack aggregate add host baremetal-hosts server-1-5
+#openstack aggregate add host baremetal-hosts server-1-6
+#openstack aggregate add host baremetal-hosts server-1-7
+#openstack aggregate add host baremetal-hosts server-1-8
 #openstack aggregate create --zone=dpdk1 dpdk1
 #openstack aggregate create --zone=dpdk2 dpdk2
 #openstack aggregate add host dpdk1 $stack_name-compute-0.localdomain
@@ -136,23 +136,39 @@ openstack router add subnet tenant1-router $subnet_id
 #openstack subnet create dpdk212-subnet --network dpdk212 --dhcp --allocation-pool start=172.16.212.2,end=172.16.212.100 --dns-nameserver 172.21.172.1 --gateway 172.16.212.1 --subnet-range 172.16.212.0/24
 
 #provider networks as admin user for non-DPDK setup
-#vlans 26,67,201-208
+#vlans 26,201-208
 source ~/vco2rc
 openstack network create internet26 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 26 --share
 openstack subnet create internet26-subnet --network internet26 --dhcp --allocation-pool start=172.21.26.5,end=172.21.26.254 --dns-nameserver 192.168.0.254 --gateway 172.21.26.1 --subnet-range 172.21.26.0/24
-openstack network create baremetal67 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 67 --share
-openstack subnet create baremetal67-subnet --network baremetal67 --dhcp --allocation-pool start=192.168.67.20,end=192.168.67.254 --dns-nameserver 192.168.0.254 --gateway 192.168.67.1 --subnet-range 192.168.67.0/24 --ip-version 4
+#openstack network create baremetal67 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 67 --share
+#openstack subnet create baremetal67-subnet --network baremetal67 --dhcp --allocation-pool start=192.168.67.20,end=192.168.67.254 --dns-nameserver 192.168.0.254 --gateway 192.168.67.1 --subnet-range 192.168.67.0/24 --ip-version 4
+openstack network create edge-ctrl201 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 201 --share
+openstack subnet create edge-ctrl201-subnet --network edge-ctrl201 --dhcp --allocation-pool start=192.168.201.5,end=192.168.201.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.201.0/24
+openstack network create edge-data202 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 202 --share
+openstack subnet create edge-data202-subnet --network edge-data202 --dhcp --allocation-pool start=192.168.202.5,end=192.168.202.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.202.0/24
+openstack network create epc-ctrl203 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 203 --share
+openstack subnet create epc-ctrl203-subnet --network epc-ctrl203 --dhcp --allocation-pool start=192.168.203.5,end=192.168.203.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.203.0/24
+openstack network create epc-data204 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 204 --share
+openstack subnet create epc-data204-subnet --network epc-data204 --dhcp --allocation-pool start=192.168.204.5,end=192.168.204.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.204.0/24
+openstack network create ims-ctrl205 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 205 --share
+openstack subnet create ims-ctrl205-subnet --network ims-ctrl205 --dhcp --allocation-pool start=192.168.205.5,end=192.168.205.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.205.0/24
+openstack network create ims-data206 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 206 --share
+openstack subnet create ims-data206-subnet --network ims-data206 --dhcp --allocation-pool start=192.168.206.5,end=192.168.206.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.206.0/24
+openstack network create gilan207 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 207 --share
+openstack subnet create gilan207-subnet --network gilan207 --dhcp --allocation-pool start=192.168.207.5,end=192.168.207.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.207.0/24
+openstack network create oam208 --provider-physical-network datacentre --provider-network-type vlan --provider-segment 208 --share
+openstack subnet create oam208-subnet --network oam208 --dhcp --allocation-pool start=192.168.208.5,end=192.168.208.254 --dns-nameserver 192.168.0.254 --subnet-range 192.168.208.0/24
 
 # Ironic in the overcloud baremetal router creation
-openstack router create baremetal67-router
-openstack router add subnet baremetal67-router baremetal67-subnet
+#openstack router create baremetal67-router
+#openstack router add subnet baremetal67-router baremetal67-subnet
 
 # Ironic in the overcloud deployment images
-openstack image create --container-format aki --disk-format aki --public --file ~/images/ironic-python-agent.kernel bm-deploy-kernel
-openstack image create --container-format ari --disk-format ari --public --file ~/images/ironic-python-agent.initramfs bm-deploy-ramdisk
+#openstack image create --container-format aki --disk-format aki --public --file ~/images/ironic-python-agent.kernel bm-deploy-kernel
+#openstack image create --container-format ari --disk-format ari --public --file ~/images/ironic-python-agent.initramfs bm-deploy-ramdisk
 
 # Ironic in the overcloud user images rhel7
-cd ~/postdeploy-scripts/
+#cd ~/postdeploy-scripts/
 #export DIB_LOCAL_IMAGE=~/postdeploy-scripts/rhel-server-7.4-x86_64-kvm.qcow2
 #disk-image-create rhel7 baremetal -o rhel-image
 #KERNEL_ID=$(openstack image create --file rhel-image.vmlinuz --public --container-format aki --disk-format aki -f value -c id rhel-image.vmlinuz)
@@ -160,11 +176,11 @@ cd ~/postdeploy-scripts/
 #openstack image create --file rhel-image.qcow2 --public --container-format bare --disk-format qcow2 --property kernel_id=$KERNEL_ID --property ramdisk_id=$RAMDISK_ID rhel-image
 
 # Ironic in the overcloud user images centos7
-export DIB_LOCAL_IMAGE=~/postdeploy-scripts/CentOS-7-x86_64-GenericCloud.qcow2.qcow2
-disk-image-create centos7 baremetal dhcp-all-interfaces grub2 -o centos-image
-KERNEL_ID=$(openstack image create --file centos-image.vmlinuz --public --container-format aki --disk-format aki -f value -c id centos-image.vmlinuz)
-RAMDISK_ID=$(openstack image create --file centos-image.initrd --public --container-format ari --disk-format ari -f value -c id centos-image.initrd)
-openstack image create --file centos-image.qcow2 --public --container-format bare --disk-format qcow2 --property kernel_id=$KERNEL_ID --property ramdisk_id=$RAMDISK_ID centos-image
+#export DIB_LOCAL_IMAGE=~/postdeploy-scripts/CentOS-7-x86_64-GenericCloud.qcow2.qcow2
+#disk-image-create centos7 baremetal dhcp-all-interfaces grub2 -o centos-image
+#KERNEL_ID=$(openstack image create --file centos-image.vmlinuz --public --container-format aki --disk-format aki -f value -c id centos-image.vmlinuz)
+#RAMDISK_ID=$(openstack image create --file centos-image.initrd --public --container-format ari --disk-format ari -f value -c id centos-image.initrd)
+#openstack image create --file centos-image.qcow2 --public --container-format bare --disk-format qcow2 --property kernel_id=$KERNEL_ID --property ramdisk_id=$RAMDISK_ID centos-image
 
 
 # Floating IP network as admin user
@@ -182,9 +198,13 @@ do
     openstack floating ip create floating
 done
 
-# instance booting
+# instance booting dpdk example
 #openstack server create --flavor dpdk-flavor.s1 --availability-zone dpdk1 --image rhel7 --nic net-id=38e99f1a-6168-4006-bfcf-15d7f1a50608 --nic net-id=50e66689-b208-4ca1-a681-55dc9db764f0 --key-name dcain vnf-0-1
 #openstack server create --flavor dpdk-flavor.s1 --availability-zone dpdk2 --image rhel7 --nic net-id=38e99f1a-6168-4006-bfcf-15d7f1a50608 --nic net-id=50e66689-b208-4ca1-a681-55dc9db764f0 --key-name dcain vnf-1-1
+
+#instance booting non-dpdk example
+#openstack server create --flavor m1.medium --image rhel7 --nic net-id=963549c3-fe2e-49a0-a312-996a0950c03f --key-name dcain dcain1
+
 }
 
 if [ $1 = "lldp" ]; then
