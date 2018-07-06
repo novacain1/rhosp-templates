66b5328220bc84662ac8fa8517fe5f672984ac20
Dave Cain <novacain1@users.noreply.github.com>
2018-07-06 10:19:54 -0400

    jul4_updates

--- RHT/deployOSP.sh
+++ RHT/deployOSP.sh
@@ -6,7 +6,8 @@ STACKNAME=rht
 # SAFETY CHECKS
 if [ "$#" -lt 1 ]; then
   echo "Pass: $0 deploy --> deploy RHOSP"
-  echo "Pass: $0 update --> update RHOSP packages"
+  echo "Pass: $0 updateUC --> update RHOSP undercloud"
+  echo "Pass: $0 updateOC --> update RHOSP overcloud"
   exit 1
 fi
 
@@ -15,20 +16,60 @@ undercloud"; exit 1)
 
 function deployRHOSP {
 time openstack overcloud deploy --templates \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-  -e ~/templates/rht.yaml \
-  -e ~/templates/network-environment.yaml \
   -e ~/templates/overcloud_images.yaml \
-  -t 120 \
+  --environment-directory ~/environments \
+  --timeout 90 \
+  --verbose \
   --stack $STACKNAME
 }
 
-function updateRHOSP {
-time yes "" | openstack overcloud update stack overcloud -i --templates \
+function updateUC {
+echo "Wiping old IPA images."
+openstack image list | awk '$2 && $2 != "ID" {print $2}' | xargs -n1 openstack image delete
+
+sudo yum -y update python-tripleoclient
+time openstack undercloud upgrade
+echo "Reboot the undercloud to update the operating system's kernel and other system packages."
+
+rm -rf  ~/images/*
+cd ~/images
+for i in /usr/share/rhosp-director-images/overcloud-full-latest-13.0.tar /usr/share/rhosp-director-images/ironic-python-agent-latest-13.0.tar; do tar -xvf $i; done
+cd ~
+
+time openstack overcloud image upload --image-path /home/stack/images
+openstack overcloud node configure $(openstack baremetal node list -c UUID -f value)
+
+openstack image list
+ls -l /httpboot
+echo "Done updating the undercloud."
+}
+
+function updateOC {
+openstack overcloud update prepare --templates \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
   -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
-  -e ~/templates/rht.yaml \
-  -e ~/templates/network-environment.yaml \
-  -e ~/templates/overcloud_images.yaml
+  -e ~/templates/overcloud_images.yaml \
+  --environment-directory ~/environments
+
+openstack overcloud update run --roles Controller
+openstack overcloud update run --roles Compute
+openstack overcloud update run --roles CephStorage
+
+openstack overcloud ceph-upgrade run --templates \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  -e ~/templates/overcloud_images.yaml \
+  --environment-directory ~/environments
+
+time openstack overcloud update converge --templates \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/ceph-ansible/ceph-ansible.yaml \
+  -e /usr/share/openstack-tripleo-heat-templates/environments/network-isolation.yaml \
+  -e ~/templates/overcloud_images.yaml \
+  --environment-directory ~/environments
+
+echo "Update complete."
 }
 
 
@@ -36,8 +77,12 @@ if [ $1 = "deploy" ]; then
     deployRHOSP
 fi
 
-if [ $1 = "update" ]; then
-    updateRHOSP
+if [ $1 = "updateUC" ]; then
+    updateUC
+fi
+
+if [ $1 = "updateOC" ]; then
+    updateOC
 fi
 
 exit 0
